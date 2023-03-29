4c5f5099b261f80053fc6f6c20e9390f22c60340
Onemoredev <akim.onemoredev@gmail.com>
2023-03-29 17:31:33 -0300

    Including some files

---
+++ src/Form.js
@@ -0,0 +1,6 @@
+
+export default function Insert(){
+    return(
+      <p>I'm a new page. That's all!</p>
+    );
+  }
---
+++ src/components/Insert.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Insert.css';
+
+
+function Insert(){
+    return (
+        <div className="insertWindow">
+        <h1>Movie Data</h1>
+        </div>
+    )
+}
+
+
+export default Insert;
